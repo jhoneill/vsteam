5e9eaefbf3c8baafabedc635a7d1281a4ecf4acf
James O'Neill <P10506111@capita.co.uk>
2020-10-30 17:56:26 +0000

    Add tests for classes

--- Tests/function/tests/Add-VSTeamWorkItemControl.Tests.ps1
+++ Tests/function/tests/Add-VSTeamWorkItemControl.Tests.ps1
@@ -123,3 +123,189 @@ Describe 'VSTeamWorkItemControl' {
       }
    }
 }
+
+#Add-VSTeamWorkItemControl uses process, field , workitem, page, and pagegroup completers
+#so it is as good a place as any to test them and their associated transform and validate classes.
+Describe 'CompletersTransformersAndValidators' {
+   BeforeAll {
+      ## Arrange
+
+      . "$PSScriptRoot\_testInitialize.ps1" $PSCommandPath
+      . "$PSScriptRoot\..\..\..\Source\Public\Get-VSTeamField.ps1"
+      . "$PSScriptRoot\..\..\..\Source\Public\Get-VSTeamWorkItemType.ps1"
+      . "$PSScriptRoot\..\..\..\Source\Public\Get-VSTeamProcess.ps1"
+      . "$PSScriptRoot\..\..\..\Source\Public\unlock-VSTeamWorkItemType.ps1"
+
+
+      # Set the account to use for testing.
+      # A normal user would do this using the Set-VSTeamAccount function.
+      [vsteam_lib.Versions]::Account = 'https://dev.azure.com/test'
+      Mock _getApiVersion { return '1.0-unitTests' }
+
+      mock Unlock-VSTeamWorkItemType {return $workitemType}
+
+      Mock Get-VSTeamField {
+            $fields = @([PSCustomObject]@{Name = "Office";ReferenceName="Custom.Office";Type="String"}
+                        [PSCustomObject]@{Name = "Notes"; ReferenceName="Custom.Notes";Type="Html"}
+                        [PSCustomObject]@{Name = "Issue";ReferenceName= "Microsoft.VSTS.Common.Issue";Type="String"}
+            )
+            if ($ReferenceName) {return  $fields.where({$_.ReferenceName -like "*$ReferenceName"})}
+            else                {return $fields}
+      }
+
+      Mock Get-VSTeamWorkItemType  {
+         $wits = Open-SampleFile "BugAndChangeReqLayout.json"
+            if ($WorkItemType) { return $wits.where( { $_.name -like $WorkItemType }) }
+            else               { return $wits }
+      }
+
+      Mock Get-VSTeamProcess {
+         $processes = @(
+            [PSCustomObject]@{Name = "Scrum";  url = 'http://bogus.none/1'; ID = "6b724908-ef14-45cf-84f8-768b5384da45" },
+            [PSCustomObject]@{Name = "Basic";  url = 'http://bogus.none/2'; ID = "b8a3a935-7e91-48b8-a94c-606d37c3e9f2" },
+            [PSCustomObject]@{Name = "CMMI";   url = 'http://bogus.none/3'; ID = "27450541-8e31-4150-9947-dc59f998fc01" },
+            [PSCustomObject]@{Name = "Agile";  url = 'http://bogus.none/4'; ID = "adcc42ab-9882-485e-a3ed-7678f01f66bc" },
+            [PSCustomObject]@{Name = "Scrum5"; url = 'http://bogus.none/5'; ID = "12345678-0000-0000-0000-000000000000" }
+         )
+         if ($name) { return $processes.where( { $_.name -like $name }) }
+         else       { return $processes }
+      }
+   }
+   context 'color' {
+      it 'Provides a set of color names'{
+         $cc = [vsteam_lib.ColorCompleter]::new()
+         $a = $cc.CompleteArgument('','','',$null,@{})
+         $a.count | should -BeGreaterThan 1
+      }
+      it 'transforms names a  color name to a hex value' {
+         $cttha = [vsteam_lib.ColorTransformToHexAttribute]::new()
+         $a = $cttha.Transform($null,'Red')
+         $a | should -be 'ff0000'
+         $a = $cttha.Transform($null,$a)
+         $a | should -be 'ff0000'
+         $a = $cttha.Transform($null,"#$a")
+         $a | should -be 'ff0000'
+         $a = $cttha.Transform($null,"notacolor")
+         $a | should -be '000000'
+      }
+   }
+
+   context 'field' {
+      beforeall {
+         [vsteam_lib.fieldCache]::Invalidate()
+         [vsteam_lib.Versions]::Account = 'https://dev.azure.com/test'
+      }
+      it 'Populates the cache for FieldCompleter operations' {
+         $fc = [vsteam_lib.FieldCompleter]::new()
+         $a = $fc.CompleteArgument('','','',$null,@{})
+         should -Invoke Get-vsteamfield -Scope it -Times 1  -Exactly
+         $a.count | should -BeGreaterThan 1
+      }
+      it 'Uses the cache for repeat FieldCompleter operations' {
+         $fc = [vsteam_lib.FieldCompleter]::new()
+         $a = $fc.CompleteArgument('','','',$null,@{})
+         should -Invoke Get-vsteamfield -Scope it -Times 0 -Exactly
+         $a.count | should -BeGreaterThan 1
+      }
+      it 'Uses the cache for FieldTransformAttribute operations' {
+         $fta = [vsteam_lib.FieldTransformAttribute]::new()
+         $a = $Fta.Transform($null,"Office")
+         should -Invoke Get-vsteamfield -Scope it -Times 0 -Exactly
+         $a       | should -beExactly "Custom.Office"
+         $b = $fta.Transform($null,@("Office","notes"))
+         $b.count | should -beExactly 2
+         $b[1]    | should -beExactly "Custom.Notes"
+         {$fta.Transform($null,"Absent")}  | should -throw
+      }
+   }
+
+   context 'Process' {
+      BeforeAll {[vsteam_lib.ProcessTemplateCache]::Invalidate()}
+      it 'Populates the cache for ProcessTemplateCompleter operations' {
+      ##act
+         $Ptc = [vsteam_lib.ProcessTemplateCompleter]::new()
+         $a   = $ptc.CompleteArgument('','','',$null,@{})
+         should -Invoke Get-vsteamProcess -Scope it -Times 1  -Exactly
+         $a.count | should -BeGreaterThan 1
+      }
+      it 'Uses the cache for repeat ProcessTemplateCompleteroperations' {
+         $Ptc = [vsteam_lib.ProcessTemplateCompleter]::new()
+         $a   = $ptc.CompleteArgument('','','',$null,@{})
+         should -Invoke Get-vsteamProcess -Scope it -Times 0  -Exactly
+         $a.count | should -BeGreaterThan 1
+      }
+      it 'Validates ProcessTemplate (or not) as expected using the ProcessTemplate cache' {
+         {Add-VSTeamWorkItemControl -ProcessTemplate scrum5 -WorkItemType ant  -ReferenceName office -WhatIf -warningAction silentlyContinue} |  should -not -Throw
+         {Add-VSTeamWorkItemControl -ProcessTemplate agile,scrum5 -WorkItemType ant  -ReferenceName office -WhatIf -warningAction silentlyContinue} |  should -not -Throw
+
+         {Add-VSTeamWorkItemControl -ProcessTemplate scrum32768 -WorkItemType bug  -ReferenceName office -WhatIf} |  should  -Throw
+         should -Invoke Get-vsteamProcess -Scope it -Times 0  -Exactly
+      }
+   }
+   context 'WorkItemType' {
+      BeforeAll {
+         # Prime the process cache with an empty list,
+         # So any name will be validated without calling Get-VSTeamProcess
+         [vsteam_lib.ProcessTemplateCache]::Update([string[]]@(), 120)
+         [vsteam_lib.WorkItemTypeCache]::Invalidate()
+         $Global:PSDefaultParameterValues["*-vsteam*:projectName"] = 'test'
+      }
+      it 'Populates the cache for WorkItemTypeCompleter operations' {
+            $witc = [vsteam_lib.WorkItemTypeCompleter]::new()
+            $a   = $witc.CompleteArgument('','','',$null,@{})
+            should -Invoke Get-vsteamWorkItemType -Scope it -Times 1  -Exactly
+            $a.count | should -BeGreaterThan 1
+      }
+      it 'Uses the cache for repeat WorkItemTypeCompleter operations' {
+            $witc = [vsteam_lib.WorkItemTypeCompleter]::new()
+            $a   = $witc.CompleteArgument('','','',$null,@{})
+            should -Invoke Get-vsteamWorkItemType -Scope it -Times 0  -Exactly
+            $a.count | should -BeGreaterThan 1
+      }
+
+      it "Doesn't use the cache for non-default process templates." {
+            $witc = [vsteam_lib.WorkItemTypeCompleter]::new()
+            $a   = $witc.CompleteArgument('','','',$null,@{ProcessTemplate='Scrum5'})
+            should -Invoke Get-vsteamWorkItemType -Scope it -Times 1  -Exactly
+            $a.count | should -BeGreaterThan 1
+      }
+   }
+
+   context 'WorkItemPage' {
+      BeforeAll {
+         # Prime the process cache with an empty list,
+         # So any name will be validated without calling Get-VSTeamProcess
+         [vsteam_lib.ProcessTemplateCache]::Update([string[]]@(), 120)
+         $Global:PSDefaultParameterValues["*-vsteam*:projectName"] = 'test'
+
+      }
+      it 'Gets WorkItem layout Pages' {
+            $pc= [vsteam_lib.PageCompleter]::new()
+            $a   = $pc.CompleteArgument('','','',$null,@{ProcessTemplate='Scrum5';WorkItemType='Bug'})
+            should -Invoke Get-vsteamWorkItemType -Scope it -Times 1  -Exactly -ParameterFilter {
+               $ProcessTemplate -eq 'Scrum5' -and $WorkItemType -eq 'Bug' -and $Expand -eq 'layout'
+            }
+            $a.count | should -BeGreaterThan 1
+            $a.CompletionText | should -contain "details"
+      }
+   }
+      context 'WorkItemPageGroup' {
+      BeforeAll {
+         # Prime the process cache with an empty list,
+         # So any name will be validated without calling Get-VSTeamProcess
+         [vsteam_lib.ProcessTemplateCache]::Update([string[]]@(), 120)
+         $Global:PSDefaultParameterValues["*-vsteam*:projectName"] = 'test'
+
+      }
+      it 'Gets WorkItem layout Page groups' {
+            $pgc= [vsteam_lib.PageGroupCompleter]::new()
+            $a   = $pgc.CompleteArgument('','','',$null,@{ProcessTemplate='Scrum5';WorkItemType='Bug';pageLabel='Details'})
+            should -Invoke Get-vsteamWorkItemType -Scope it -Times 1  -Exactly -ParameterFilter {
+               $ProcessTemplate -eq 'Scrum5' -and $WorkItemType -eq 'Bug' -and $Expand -eq 'layout'
+            }
+            $a.count | should -BeGreaterThan 1
+            $a.CompletionText | should -contain "Deployment"
+      }
+   }
+
+}
